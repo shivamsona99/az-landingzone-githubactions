ad4583c38eeb83f5ea5310eae1ed044eb69e3e71
shivamsona99 <shivamsona99@gmail.com>
2026-08-16 04:07:34 -0700

    Update Terraform configuration

--- rg.tf
+++ rg.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "rgs1" {
 }
 
 resource "azurerm_virtual_network" "vnets" {
-  depends_on = [ azurerm_resource_group.rgs1 ]
+  depends_on = [ azurerm_resource_group.rgs ]
   name                = "shivam-vnets"
   location            = "centralindia"
   resource_group_name = "shivamsona"
@@ -16,9 +16,9 @@ resource "azurerm_virtual_network" "vnets" {
 }
 # Both the resource group and virtual network must be in the same region for best practics
 resource "azurerm_virtual_network" "name" {
-  depends_on = [ azurerm_resource_group.rgs ]
+  depends_on = [ azurerm_resource_group.rgs1]
   name                = "shivam-vnets"
   location            = "centralindia"
-  resource_group_name = "shivamsona"
+  resource_group_name = "shivamsonasingh"
   address_space       = ["10.0.0.0/16"]
 }
