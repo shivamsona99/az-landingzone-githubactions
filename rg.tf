ba39d7bd9e7aed5de9ec77d977cebe0d91089171
shivamsona99 <shivamsona99@gmail.com>
2026-08-16 04:02:29 -0700

    Create separate resource group and add dependency

--- rg.tf
+++ rg.tf
@@ -2,9 +2,13 @@ resource "azurerm_resource_group" "rgs" {
   name     = "shivamsona"
   location = "centralindia"
 }
+resource "azurerm_resource_group" "rgs1" {
+  name     = "shivamsonasingh"
+  location = "centralindia"
+}
 
 resource "azurerm_virtual_network" "vnets" {
-  depends_on          = [azurerm_resource_group.rgs]
+  depends_on = [ azurerm_resource_group.rgs1 ]
   name                = "shivam-vnets"
   location            = "centralindia"
   resource_group_name = "shivamsona"
@@ -12,6 +16,7 @@ resource "azurerm_virtual_network" "vnets" {
 }
 # Both the resource group and virtual network must be in the same region for best practics
 resource "azurerm_virtual_network" "name" {
+  depends_on = [ azurerm_resource_group.rgs ]
   name                = "shivam-vnets"
   location            = "centralindia"
   resource_group_name = "shivamsona"
